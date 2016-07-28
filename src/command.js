eb8a901e7fbc1899dfa26126432b374cbf8566c1
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-28 16:53:06 +0200

    Formatting

--- src/command.js
+++ src/command.js
@@ -6,9 +6,9 @@ module.exports = ({ heimdall }) => async ({ args, reply }) => {
       .filter('search', args);
   const assets = await heimdall(query);
   if (assets.length) {
-    const lines = assets.map(asset => reply.link(`https://store.maxdome.de/${asset.id}`, asset.title));
-    lines.push(reply.link(`https://store.maxdome.de/suche?search=${args}`, 'show all...'));
-    reply.send(lines);
+    const texts = assets.map(asset => reply.link(`https://store.maxdome.de/${asset.id}`, asset.title));
+    texts.push(reply.link(`https://store.maxdome.de/suche?search=${args}`, 'show all...'));
+    reply.send(texts);
   } else {
     reply.send(`no results found for "${args}"`);
   }
